d7a8e35fedb8f91fbc3a58e5dd273a1b0ad92800
Tattsum <tatsuma.kano@yappli.co.jp>
2020-11-28 04:42:38 +0900

    first commit

---
+++ NoFileOpen_test.go
@@ -0,0 +1,14 @@
+package NoFileOpen_test
+
+import (
+	"testing"
+
+	"github.com/Tattsum/NoFileOpen"
+	"golang.org/x/tools/go/analysis/analysistest"
+)
+
+// TestAnalyzer is a test for Analyzer.
+func TestAnalyzer(t *testing.T) {
+	testdata := analysistest.TestData()
+	analysistest.Run(t, testdata, NoFileOpen.Analyzer, "a")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Tattsum/NoFileOpen
+
+go 1.15
